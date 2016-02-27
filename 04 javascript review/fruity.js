97214ca804cc803f5f138fe8a83b532d77637e22
amaeder <amaeder@gmail.com>
2016-02-27 10:35:17 -0500

    04 Homework Attempt
    UGH!!!!!

---
+++ 04 javascript review/fruity.js
@@ -0,0 +1,12 @@
+   var fruity = [
+            "apple",
+            "banana",
+            "kiwi",
+            "orange",
+            "mango",
+            "cranberry",
+            "grape",
+        ];
+        
+    var random = Math.floor(Math.random(fruity);
+    document.write(random);
