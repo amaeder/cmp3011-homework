f3e3aa6a9d4a8a9aadbc64f96ace06fda637c177
amaeder <amaeder@gmail.com>
2016-03-05 09:35:53 -0500

    Got email to kind of work. Email not working. Haven't tried to do Password

--- 05 registration form/js.js
+++ 05 registration form/js.js
@@ -8,17 +8,17 @@ function validateForm() {
     var pwd1_check = document.getElementById("password").value;
     var pwd2_check = document.getElementById("password2").value;
 
-    var name_check_err = document.getElementById("username_err").textcontent;
+    var username_err = document.getElementById("username_err").textcontent;
     var email_err = document.getElementById("email_err").textcontent;
     var password_err = document.getElementById("password_err").textcontent;
 
-    var msgname_check_err = "This must be longer than 2 characters!";
+    var msgnamecheck_err = "This must be longer than 2 characters!";
     var msgemail_err = "This must be longer than 5 characters!";
     var msgpassword_err = "This must be longer than 10 characters!";
 
 
     if (name_check.length < 3) {
-        document.getElementById("username_err").textcontent = msgname_check_err;
+        document.getElementById("username_err").textcontent = msgnamecheck_err;
 
         noErrors = false;
     }
@@ -27,6 +27,19 @@ function validateForm() {
         document.getElementById("username_err").textcontent = " ";
 
     }
+
+    if (email_check.length < 6) {
+        document.getElementById("email_err").textContent = msgemail_err;
+
+        noErrors = false;
+
+    }
+
+    else {
+        document.getElementById("email_err").textcontent = " ";
+
+    }
+
     return noErrors;
 
 }
