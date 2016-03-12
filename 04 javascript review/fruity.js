7b3d43dfb462ae28a7fa640b866be3621e952895
amaeder <smfa@A-EVELYN-GRANVILLE.local>
2016-03-12 15:58:31 -0500

    fruit loops

--- 04 javascript review/fruity.js
+++ 04 javascript review/fruity.js
@@ -5,8 +5,50 @@
             "orange",
             "mango",
             "cranberry",
-            "grape",
+            "grape"
         ];
-        
-    var random = Math.floor(Math.random(fruity);
-    document.write(random);
+
+
+
+   for (  fruit_index = 0    ;   fruit_index < fruity.length    ;    fruit_index++    ) {
+
+
+       //action 1
+       //
+       //action 2
+
+       document.write (fruity [fruit_index] + " " );
+
+   }
+
+   //for (  initialization    ;   test    ;    post loop action    ) {
+//
+//    action 1
+//
+//    action 2
+//
+//
+//
+//}
+
+   //document.write (fruity[0]);
+   //
+   //
+   //
+   //document.write (fruity[1]);
+   //
+   //document.write (fruity[2]);
+   //
+   //document.write (fruity[3]);
+   //
+   //document.write (fruity[4]);
+
+
+    //var random = Math.floor(Math.random(fruity);
+    //document.write(random);
+
+   function print_word (word){
+
+       alert (word);
+
+   }
