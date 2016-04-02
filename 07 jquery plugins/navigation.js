3eadeef5ebbe7d47d27fe2bc83f4597cda1086f0
amaeder <amaeder@gmail.com>
2016-04-02 11:51:11 -0400

    Not working... yet
    A lot more work to do, but somewhat understand at least pulling js plugin into my pages.

---
+++ 07 jquery plugins/navigation.js
@@ -0,0 +1,10 @@
+/*! formstone v1.0.1 [navigation.js] 2016-03-25 | MIT License | formstone.it */
+
+$().ready(function() {
+
+    $("nav").navigation(    {
+        "gravity": "left",
+        "type": "overlay"
+    });
+
+});
